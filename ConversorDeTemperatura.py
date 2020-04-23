b7ecda38f4e60172635f83cce5ff48aeec12bdad
Marcio Kina <marciokina@gmail.com>
2020-04-23 17:31:00 -0300

    primeiro arquivo

---
+++ ConversorDeTemperatura.py
@@ -0,0 +1,11 @@
+#curso "o que é ciência da comutação" USP/Coursera
+#segunda Semana - entrada de dados (python)
+
+#inserir dados em fahrenheit
+temperaturaFahrenheit = input("Digte uma temperatura em fahrenheit: ")
+
+#conversão dos dados em fahrenheit para celsius
+temperaturaCelsius = (float(temperaturaFahrenheit)-32)*5/9
+
+#apresentação dos dados em celsius
+print("A temperatura em celsius é", temperaturaCelsius)
